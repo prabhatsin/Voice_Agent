a2c30dee63378be082ac757e3856852de2eb3411
prabhatsin <prabhat.singh112244@gmail.com>
2026-09-16 14:45:49 +0530

    while True in ws_server.py

--- src/ws_server.py
+++ src/ws_server.py
@@ -5,10 +5,11 @@ import websockets
 
 async def handler(websocket):
     print("client_connected")
-    message = await websocket.recv()
-    print("Client:", message)
 
-    await websocket.send("Hello Client")
+    while True:
+        message = await websocket.recv()
+        print("Client:", message)
+        await websocket.send("Received: "+ message)
 
 async def main():
     async with websockets.serve(handler,"localhost",8765): #websockets.serve(handler, host, port, **kwargs)
